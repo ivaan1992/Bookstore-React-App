c9a6015abb6953a3e78b2138ef1bf301a2d36c5b
ivaan1992 <ivaan.lg.92@gmail.com>
2022-03-25 10:22:21 -0600

    Finish the books section

---
+++ src/components/UI/Form.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Form = () => (
+  <form>
+    <input type="text" id="book" placeholder="Add a book..." />
+    <input type="text" id="author" placeholder="Author" />
+    <button type="button">Add book</button>
+  </form>
+);
+
+export default Form;
