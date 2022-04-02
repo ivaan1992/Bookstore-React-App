ea75a9947828eaa1dddd2f19a043b0b6f90050eb
ivaan1992 <ivaan.lg.92@gmail.com>
2022-04-02 01:15:59 -0600

    Connect Redux store

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
 import './App.css';
-import { Routes, Route } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import store from './redux/configureStore';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Books from './components/Books';
 import Categories from './components/Categories';
 
 const App = () => (
-  <div className="App">
+  <BrowserRouter>
     <Header />
-    <Provider store={store}>
-      <Routes>
-        <Route path="/" element={<Books />} />
-        <Route path="/Categories" element={<Categories />} />
-      </Routes>
-    </Provider>
-  </div>
+    <Routes>
+      <Route path="/" element={<Books />} />
+      <Route path="/Categories" element={<Categories />} />
+    </Routes>
+  </BrowserRouter>
 );
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import App from './App';
+import store from './redux/configureStore';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <Provider store={store}>
     <App />
-  </BrowserRouter>,
+  </Provider>,
   document.getElementById('root'),
 );
